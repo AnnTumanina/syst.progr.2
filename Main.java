25be40f57079815dc977ee8c45d4245d1651130a
AnnTumanina <98632330+AnnTumanina@users.noreply.github.com>
2022-09-17 12:49:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 package com.company;
 
+import java.util.Objects;
 
 class AnimalThread extends Thread {
     public String Name;
-    public int Priority;
 
-    AnimalThread(String name, int priority) {
+    AnimalThread(String name) {
         this.Name = name;
-        this.Priority = priority;
     }
 
 
@@ -15,23 +14,30 @@ class AnimalThread extends Thread {
     public void run() {
         System.out.println("На старте " + getName());
         for (int i = 0; i <= 1000; i += 100) {
-                System.out.println(getName() + ": " + i + " метров.");
+            System.out.println(getName() + ": " + i + " метров.");
+            if(i==400){
+                try{
+                    System.out.println(getName() + ": " + i + " метров.");
+                    if(Objects.equals(getName(), "Кролик")){
+                        System.out.println("КРОЛИК УСТАЛ");
+                        join(1);
+                    }
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+            }
         }
     }
 }
 
 class Main {
-
     public static void main(String[] args) {
-
-        AnimalThread rabbit = new AnimalThread("Кролик", 1);
+        AnimalThread rabbit = new AnimalThread("Кролик");
         rabbit.setName(rabbit.Name);
-        rabbit.setPriority(rabbit.Priority);
-        AnimalThread turtle = new AnimalThread("Черепаха", 10);
+        AnimalThread turtle = new AnimalThread("Черепаха");
         turtle.setName(turtle.Name);
-        turtle.setPriority(turtle.Priority);
         turtle.start();
         rabbit.start();
-
     }
 }
